aacab99d71cda61943532879969a199558447532
Han Shao <hshao@oberlin.edu>
2019-09-23 19:26:38 -0400

    print CLSs shape

--- run_race.py
+++ run_race.py
@@ -294,6 +294,7 @@ def create_model(bert_config, is_training, four_options, labels, num_labels,
 
 
     CLSs = tf.stack(CLSs)
+    print('CLSs shape:', tf.shape(CLSs))
 
     output_layer = tf.layers.dense(CLSs, num_labels, activation=tf.tanh)
     print('output_layer shape:', tf.shape(output_layer))
