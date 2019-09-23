50946552e2ec64994c520ff90eea3083f3a56bbb
Han Shao <hshao@oberlin.edu>
2019-09-23 19:13:36 -0400

    delete dtype

--- run_race.py
+++ run_race.py
@@ -415,8 +415,7 @@ def input_fn_builder(features, seq_length, is_training, drop_remainder):
         d = tf.data.Dataset.from_tensor_slices({
             "four_options":
                 tf.constant(
-                    all_four_options, shape=[num_examples, 4, seq_length],
-                    dtype=Option),
+                    all_four_options, shape=[num_examples, 4, seq_length]),
             "label_ids":
                 tf.constant(all_label_ids, shape=[num_examples], dtype=tf.int32),
         })
