5e8babd7d12bf2c6fb4e8f3079786c09ebba0903
Han Shao <hshao@oberlin.edu>
2019-09-23 23:15:25 -0400

    add race.sh

---
+++ race.sh
@@ -0,0 +1,16 @@
+export PATH=/usr/local/cuda-9.0/bin:$PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-9.0/lib64/
+BERT_BASE_DIR='BERT_BASE/uncased_L-12_H-768_A-12'
+
+python3 run_race.py \
+ --data_dir=RACE_S/train/middle \
+ --do_lower_case=True \
+ --output_dir=RACE_S_output \
+ --do_train=True \
+ --vocab_file=$BERT_BASE_DIR/vocab.txt \
+ --bert_config_file=$BERT_BASE_DIR/bert_config.json \
+ --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
+ --max_seq_length=128 \
+ --train_batch_size=4 \
+ --learning_rate=2e-5 \
+ --num_train_epochs=3.0 \
