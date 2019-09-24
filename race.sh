7d0321534536b43578f6e9d368c3675272d5df83
Han Shao <hshao@oberlin.edu>
2019-09-23 23:47:34 -0400

    test on remote

--- race.sh
+++ race.sh
@@ -3,10 +3,11 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-9.0/lib64/
 BERT_BASE_DIR='BERT_BASE/uncased_L-12_H-768_A-12'
 
 python3 run_race.py \
- --data_dir=RACE/train/middle \
+ --data_dir=RACE \
  --do_lower_case=True \
  --output_dir=RACE_output \
  --do_train=True \
+ --do_eval=True \
  --vocab_file=$BERT_BASE_DIR/vocab.txt \
  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
