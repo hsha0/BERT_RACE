afec89b6f85b0fa99617fc11e402fea47f829254
Han Shao <hshao@oberlin.edu>
2019-09-23 23:31:14 -0400

    customize for online gpu

--- race.sh
+++ race.sh
@@ -3,14 +3,14 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-9.0/lib64/
 BERT_BASE_DIR='BERT_BASE/uncased_L-12_H-768_A-12'
 
 python3 run_race.py \
- --data_dir=RACE_S/train/middle \
+ --data_dir=RACE/train/middle \
  --do_lower_case=True \
- --output_dir=RACE_S_output \
+ --output_dir=RACE_output \
  --do_train=True \
  --vocab_file=$BERT_BASE_DIR/vocab.txt \
  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
- --max_seq_length=128 \
- --train_batch_size=4 \
+ --max_seq_length=512 \
+ --train_batch_size=32 \
  --learning_rate=2e-5 \
  --num_train_epochs=3.0 \
