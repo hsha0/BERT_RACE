8cf339f752ff5b028c1019a5bbfc60b1f94bcfd2
Han Shao <hshao@oberlin.edu>
2019-09-29 19:57:38 -0400

    test on remote

--- race.sh
+++ race.sh
@@ -13,9 +13,9 @@ python3 run_race.py \
  --vocab_file=$BERT_BASE_DIR/vocab.txt \
  --bert_config_file=$BERT_BASE_DIR/bert_config.json \
  --init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
- --max_seq_length=512 \
+ --max_seq_length=384 \
  --train_batch_size=6 \
- --eval_batch_size=4 \
+ --eval_batch_size=8 \
  --learning_rate=2e-5 \
  --num_train_epochs=3.0 \
  --use_tpu=True \
