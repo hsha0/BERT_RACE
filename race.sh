3143b79ebf87ef18bebb52809d12d117bc8f9dd1
Han Shao <hshao@oberlin.edu>
2019-09-30 15:27:52 -0400

    test on remote

--- race.sh
+++ race.sh
@@ -4,8 +4,10 @@
 current_time=$(date "+%Y_%m_%d-%H_%M_%S")
 BERT_BASE_DIR='BERT_BASE/uncased_L-12_H-768_A-12'
 BERT_GC='gs://bert_sh/BERT_BASE/uncased_L-12_H-768_A-12'
+DATA_PATH='https://drive.google.com/open?id=1DIRtK_1VwxID2RhAqOLmrrjqSO4qdy6-'
+
 python3 run_race.py \
- --data_dir=$1 \
+ --data_dir=$DATA_PATH \
  --do_lower_case=True \
  --output_dir=gs://bert_sh/$1_$current_time \
  --do_train=True \
