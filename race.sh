ed553d0f5fc199bcfb283dd92869642df9d8e7c0
Han Shao <hshao@oberlin.edu>
2019-10-06 14:26:04 -0400

    run predict with seed 15569

--- race.sh
+++ race.sh
@@ -2,24 +2,27 @@
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-9.0/lib64/
 
 current_time=$(date "+%Y_%m_%d-%H_%M_%S")
+
 BERT_BASE_DIR='BERT_BASE/uncased_L-12_H-768_A-12'
 BERT_GC='gs://bert_sh/BERT_BASE/uncased_L-12_H-768_A-12'
 DATA_PATH='/content/RACE'
 
 python3 run_race.py \
+ --seed=15569 \
  --data_dir=$DATA_PATH \
  --do_lower_case=True \
- --output_dir=gs://bert_sh/RACE_$current_time \
+ --output_dir=gs://bert_sh/predict_10seed/RACE_$current_time \
  --do_train=True \
  --do_eval=True \
+ --do_predict=True
  --task_name=middle \
  --vocab_file=$BERT_GC/vocab.txt \
  --bert_config_file=$BERT_GC/bert_config.json \
  --init_checkpoint=$BERT_GC/bert_model.ckpt \
  --max_seq_length=384 \
- --train_batch_size=32 \
+ --train_batch_size=64 \
  --eval_batch_size=8 \
  --learning_rate=5e-5 \
  --num_train_epochs=3.0 \
  --use_tpu=True \
- --tpu_name='grpc://10.126.232.162:8470'
+ --tpu_name='grpc://10.78.160.242:8470'
