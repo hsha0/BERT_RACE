c562f3619468d73bbe7004c0a21dd8aad8f6298c
Han Shao <hshao@oberlin.edu>
2019-10-06 14:54:31 -0400

    edit race.sh

--- race.sh
+++ race.sh
@@ -3,15 +3,17 @@
 
 current_time=$(date "+%Y_%m_%d-%H_%M_%S")
 
+SEED=15569
+
 BERT_BASE_DIR='BERT_BASE/uncased_L-12_H-768_A-12'
 BERT_GC='gs://bert_sh/BERT_BASE/uncased_L-12_H-768_A-12'
 DATA_PATH='/content/RACE'
 
 python3 run_race.py \
- --seed=15569 \
+ --seed=SEED \
  --data_dir=$DATA_PATH \
  --do_lower_case=True \
- --output_dir=gs://bert_sh/predict_10seed/RACE_$current_time \
+ --output_dir=gs://bert_sh/predict_10seed/RACE_$SEED_$current_time \
  --do_train=True \
  --do_eval=True \
  --do_predict=True \
