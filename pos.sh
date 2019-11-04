44e4bf3bc2f63f57caadc7eba99378c4c4365516
Han Shao <hshao@oberlin.edu>
2019-11-04 10:17:18 -0500

    rerun 22001

--- pos.sh
+++ pos.sh
@@ -1,16 +1,16 @@
 #export PATH=/usr/local/cuda-9.0/bin:$PATH
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-9.0/lib64/
-
-TPU_NAME='grpc://10.9.168.66:8470'
+#12345 66666 22001 78925 11982 67877 987123 65492 98045 652343
+TPU_NAME='grpc://10.109.101.106:8470'
 
 BERT_GC='gs://bert_sh/BERT_LARGE/wwm_cased_L-24_H-1024_A-16'
 DATA_PATH='/content/POS'
 
 
 
-declare -a SEEDS=(12345 66666 22001 78925 11982 67877 987123 65492 98045 652343)
+declare -a SEEDS=(22001)
 
-for run in $(seq 1 10)
+for run in $(seq 1 1)
 do
     current_time=$(date "+%y%m%d-%H%M%S")
     SEED=${SEEDS[$run-1]}
