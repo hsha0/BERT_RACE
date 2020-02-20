2e4f6ab22bff85aeadd45eb7dd0f1625fe91232c
Han Shao <hshao@oberlin.edu>
2020-02-20 03:16:48 -0500

    bert glue

--- glue_bert.sh
+++ glue_bert.sh
@@ -2,7 +2,7 @@ TPU_NAME='grpc://10.50.82.74:8470'
 BERT_GC='gs://bert_sh'
 INIT_CKPT='gs://electra/electra_pretrain/bert_small_seq128_lr5e-4/model.ckpt-237000'
 SEED=12345
-TASK_INDEX=0
+TASK_INDEX=2
 
 TASKS=(MRPC CoLA MNLI SST-2 QQP QNLI WNLI RTE STS-B)
 LRS=(3e-4 3e-4 3e-4 3e-4 3e-4 3e-4 3e-4 3e-4 3e-4)
