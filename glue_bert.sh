e7a336b5434bdff57cab8e59b0262abed19f9233
Han Shao <hshao@oberlin.edu>
2020-03-08 16:06:52 -0400

    glue 163k

--- glue_bert.sh
+++ glue_bert.sh
@@ -1,4 +1,4 @@
-TPU_NAME='grpc://10.75.205.162:8470'
+TPU_NAME='grpc://10.75.27.146:8470'
 BERT_GC='gs://bert_sh'
 INIT_CKPT='gs://electra/electra_pretrain/bert_small_seq128_lr5e-4_lamb_163k/model.ckpt-163000'
 SEED=$$
